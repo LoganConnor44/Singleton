cb231e3f1a1c85fa439d894830358aac4caae234
Logan Connor <loganconnor44@gmail.com>
2017-12-17 17:07:37 -0500

    fixing issues

--- src/Policy.php
+++ src/Policy.php
@@ -1,1 +1,39 @@
 <?php
+namespace Singleton;
+
+/**
+ *
+ */
+class Policy {
+
+	/**
+	 * @var integer
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 */
+	private $insured;
+
+	/**
+	 * Sets object properties.
+	 *
+	 * @param integer $id			 The id of the insured.
+	 * @param string  $nameOfInsured The name of the insured.
+	 */
+	public function __construct(int $id, string $nameOfInsured) {
+		$this->id = $id;
+		$this->insured = $nameOfInsured;
+	}
+
+	/**
+	 * Returns the name of the insured.
+	 *
+	 * @return string
+	 */
+	public function getInsuredName() : string {
+		return $this->insured;
+	}
+
+}
